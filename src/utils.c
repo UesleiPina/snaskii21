fcb385b247046b215ffc0d11328599fd727f7e8f
Ueslei Pina <uesleipina@usp.br>
2021-12-27 15:17:43 -0300

    Narrowing conversion from 'long' to signed type 'int' is implementation-defined

--- src/utils.c
+++ src/utils.c
@@ -35,13 +35,13 @@ int timeval_subtract (struct timeval *result, struct timeval *x,
                       struct timeval *y) {
   /* Perform the carry for the later subtraction by updating y. */
   if (x->tv_usec < y->tv_usec) {
-    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+    long nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
     y->tv_usec -= 1000000 * nsec;
     y->tv_sec += nsec;
   }
 
   if (x->tv_usec - y->tv_usec > 1000000) {
-    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+    long nsec = (x->tv_usec - y->tv_usec) / 1000000;
     y->tv_usec += 1000000 * nsec;
     y->tv_sec -= nsec;
   }
